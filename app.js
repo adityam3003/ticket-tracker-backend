803104d3dd355bf3795045b24c10864653ae8204
Aditya <mehtaaditya030@gmail.com>
2021-11-28 21:30:45 -0800

    Test Cases

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express()
 app.use(cors())
 app.get('/gettickets',async(req,res)=>{
     try{
-      const data = await axios.get(process.env.ZENDESK_API+`?per_page=25&page=${req.query.page || 1}`,{
+        const data = await axios.get(`https://zendeskcodingchallenge1566.zendesk.com/api/v2/tickets.json?per_page=25&page=${req.query.page || 1}`,{
         headers: {
           authorization: 'Basic bWVodGFhZGlAdXNjLmVkdTpJYnNheWFAMzAwMzE5OTg='
         }
       })
       if (data.status == 200 && data.data && data.data.tickets){
-        res.set('Access-Control-Allow-Origin', '*');       
+        res.set('Access-Control-Allow-Origin', '*');
         return res.status(200).json({
           tickets:data.data.tickets,
           count:data.data.count,
@@ -20,7 +20,6 @@ app.get('/gettickets',async(req,res)=>{
       }
     }
     catch(e){
-      console.log(e)
       return res.status(500)
     }
 })
--- tests/tickets.test.js
+++ tests/tickets.test.js
@@ -1,10 +1,22 @@
-const request = require('supertest')
+const supertest = require('supertest')
 const app = require('../app')
-const axios = require('axios')
-test('Should get task for user 1', async () => {
-    const response = await request(app)
-        .get('/gettickets')
-        .send()
-        .expect(200)
-    console.log(response)
+jest.useFakeTimers()
+const moxios = require('moxios')
+const {mockRequestJSON,expectedResponse} = require('./fixtures/test.json')
+const request = supertest(app)
+beforeAll(() => {
+    moxios.install();
+});
+test('Check API Response', async () => {
+    moxios.stubRequest(`https://zendeskcodingchallenge1566.zendesk.com/api/v2/tickets.json?per_page=25&page=1`, {
+        status: 200,
+        response: mockRequestJSON,
+        headers: {
+            authorization: 'Basic bWVodGFhZGlAdXNjLmVkdTpJYnNheWFAMzAwMzE5OTg='
+        }
+    })
+    
+    request.get('/gettickets').end((err,res)=>{
+        expect(res.body.tickets).toStrictEqual(expectedResponse.tickets)
+    })
 })
